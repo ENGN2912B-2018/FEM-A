408d85f89005c60e41cba516ff692b40a410156e
jackakarafotas <jackakarafotas@gmail.com>
2018-11-30 17:30:23 -0500

    finished hyperbolic PDE class - need to use this pde and conv_diff to make asian option pde

---
+++ src/FEM/PDEs/Hyperbolic.h
@@ -0,0 +1,24 @@
+#ifndef CONVECTIONDIFFUSION_H
+#define CONVECTIONDIFFUSION_H
+
+
+class HyperbolicPDE {
+public:
+	// Constructor / Destructor
+	HyperbolicPDE() {}
+	virtual ~HyperbolicPDE() {}
+
+	// Define these in children function
+	virtual double source_param(double t, double x) const = 0;
+	virtual double solution_param(double t, double x) const = 0;
+	virtual double convection_param(double t, double x) const = 0;
+
+	virtual double left_boundary(double t, double x) const = 0;
+	virtual double right_boundary(double t, double x) const = 0;
+
+	virtual double initial_condition(double x) const = 0;
+};
+
+
+
+#endif
